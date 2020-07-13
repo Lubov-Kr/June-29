8c06b12b451f1945f54c30a2741881a7de716cdf
Liubov Pepenko <64948367+Lubov-Kr@users.noreply.github.com>
2020-07-13 22:31:34 +0300

    somthin new

---
+++ test/specs/basic.js
@@ -0,0 +1,19 @@
+const {expect} = require('chai');
+
+describe('Login', () => {
+    before(() => {
+        browser.maximizeWindow();
+        browser.url('https://mail.ru/');
+    });
+
+    it('should enter username and password', function () {
+        const inputName = $('#mailbox .i-no-right-radius');
+        inputName.setValue('milisal');
+        $('.o-control[type=submit]').click();
+        browser.pause(3000);
+        $('.mailbox__input_password').setValue('ghjPhtdibqrepm13Vf');
+        browser.pause(3000);
+        $('input.o-control').click();
+    });
+
+})
